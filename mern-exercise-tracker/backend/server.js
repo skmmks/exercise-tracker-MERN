a6e89e834d79ad295ec3c80314aa5387c0c513d3
Steve Min <stevekmin@gmail.com>
2020-07-08 08:00:52 -0700

    add connection to mongodb atlas

--- mern-exercise-tracker/backend/server.js
+++ mern-exercise-tracker/backend/server.js
@@ -1,6 +1,13 @@
 const express = require('express');
 const cors = require('cors');
 
+const uri = process.env.ATLAS_URI;
+mongoose.connect(uri, { useNewUrlParser: true, useCreateIndex: true });
+const connection = mongoose.connection;
+connection.once('open', () => {
+  console.log('MongoDB database connection established successfully');
+});
+
 require('dotenv').config();
 
 const app = express();
