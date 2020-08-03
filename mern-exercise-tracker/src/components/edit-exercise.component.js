a66a0814575f54433b51f5f99b5f7da56c8d076c
Steve Min <stevekmin@gmail.com>
2020-08-03 08:14:12 -0700

    add component did mount function to fetch B.E. user info

--- mern-exercise-tracker/src/components/edit-exercise.component.js
+++ mern-exercise-tracker/src/components/edit-exercise.component.js
@@ -21,6 +21,19 @@ export default class EditExercise extends Component {
       users: [],
     };
   }
+  componentDidMount() {
+    axios.get('http://localhost:5000/exercises/'+this.props.match.params.id)
+      .then(response => {
+        this.setState({
+          username: response.data.username,
+          description: response.data.description,
+          duration: response.data.duration,
+          date: new Date(response.data.date)
+        })   
+      })
+      .catch(function (error) {
+        console.log(error);
+      })
   render() {
     return (
       <div>
