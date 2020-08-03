44ad1dfd2ed8d0f766e3857c3a43a6e4d8777c83
Steve Min <stevekmin@gmail.com>
2020-08-03 08:18:56 -0700

    modify component did mount function to GET users

--- mern-exercise-tracker/src/components/edit-exercise.component.js
+++ mern-exercise-tracker/src/components/edit-exercise.component.js
@@ -22,18 +22,29 @@ export default class EditExercise extends Component {
     };
   }
   componentDidMount() {
-    axios.get('http://localhost:5000/exercises/'+this.props.match.params.id)
-      .then(response => {
+    axios
+      .get('http://localhost:5000/exercises/' + this.props.match.params.id)
+      .then((response) => {
         this.setState({
           username: response.data.username,
           description: response.data.description,
           duration: response.data.duration,
-          date: new Date(response.data.date)
-        })   
+          date: new Date(response.data.date),
+        });
       })
       .catch(function (error) {
         console.log(error);
+      });
+
+    axios
+      .get('http://localhost:5000/users/')
+      .then((response) => {
+        this.setState({ users: response.data.map((user) => user.username) });
       })
+      .catch((error) => {
+        console.log(error);
+      });
+  }
   render() {
     return (
       <div>
