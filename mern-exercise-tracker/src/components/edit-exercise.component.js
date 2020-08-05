47f07c0cec0f56dcd71656e2842c4888db7c6c96
Steve Min <stevekmin@gmail.com>
2020-08-05 08:06:17 -0700

    add rendered ui component on edit-exercise component

--- mern-exercise-tracker/src/components/edit-exercise.component.js
+++ mern-exercise-tracker/src/components/edit-exercise.component.js
@@ -89,7 +89,48 @@ export default class EditExercise extends Component {
   render() {
     return (
       <div>
-        <p>You are on the Edit Exercise component!</p>
+        <h3>Edit Exercise Log</h3>
+        <form onSubmit={this.onSubmit}>
+          <div className='form-group'>
+            <label>Username: </label>
+            <select
+              ref='userInput'
+              className='form-control'
+              value={this.state.username}
+              onChange={this.onChangeUsername}
+            >
+              {this.state.users.map(function (user) {
+                return (
+                  <option key={user} value={user}>
+                    {user}
+                  </option>
+                );
+              })}
+            </select>
+          </div>
+          <div className='form-group'>
+            <label>Description: </label>
+            <input
+              type='text'
+              required
+              className='form-control'
+              value={this.state.description}
+              onChange={this.onChangeDescription}
+            />
+          </div>
+          <div className='form-group'>
+            <label>Duration (in minutes): </label>
+            <input type='text' className='form-control' value={this.state.duration} onChange={this.onChangeDuration} />
+          </div>
+          <div className='form-group'>
+            <label>Date: </label>
+            <DatePicker selected={this.state.date} onChange={this.onChangeDate} />
+          </div>
+
+          <div className='form-group'>
+            <input type='submit' value='Edit Exercise Log' className='btn btn-primary' />
+          </div>
+        </form>
       </div>
     );
   }
